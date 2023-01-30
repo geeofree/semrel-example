63a2305930a39210132d3e716b5fa7d370dc1084
Geo Galagaran <geeofree@proton.me>
2023-01-30 11:32:54 +0800

    feat: add `mul` function
    Add function for multiplying two numbers.

--- src/math.js
+++ src/math.js
@@ -14,7 +14,16 @@ const add = (a, b) => a + b;
  **/
 const sub = (a, b) => a - b;
 
+/**
+ * Multiples two numbers
+ * @param {number} a
+ * @param {number} b
+ * @returns {number} a * b
+ **/
+const mul = (a, b) => a * b;
+
 module.exports = {
   add,
   sub,
+  mul,
 };
--- src/math.test.js
+++ src/math.test.js
@@ -8,4 +8,8 @@ describe("Math", () => {
   test("sub(5, 3) = 2", () => {
     expect(sub(5, 3)).toBe(2);
   });
+
+  test("sub(5, 2) = 10", () => {
+    expect(sub(5, 2)).toBe(10);
+  });
 });
