b6d53ee0a81cfe42d2bbee753f8d6a763fa88728
Geo Galagaran <geeofree@proton.me>
2023-01-30 11:18:34 +0800

    feat(MATH-02): add sub function
    Implement `sub` function for subtracting two numbers.

--- src/math.test.js
+++ src/math.test.js
@@ -1,7 +1,11 @@
-const { add } = require("./math");
+const { add, sub } = require("./math");
 
 describe("Math", () => {
   test("add(1, 1) = 2", () => {
     expect(add(1, 1)).toBe(2);
+  });
+
+  test("sub(5, 3) = 2", () => {
+    expect(sub(5, 3)).toBe(2);
   });
 });
